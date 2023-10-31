c2d85c79b522d7dd27296197fe4ee69a528091a5
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-10-30 21:29:40 -0300

    update

---
+++ script.js
@@ -0,0 +1,265 @@
+// 1: Crie um programa que imprime os números de 1 a 10.
+function exercicio1() {
+  for (let i = 1; i < 11; i++) {
+    console.log(i);
+  }
+}
+
+// exercicio1();
+
+// 2: Crie um programa que imprime os números pares de 1 a 20.
+function exercicio2() {
+  for (let i = 1; i < 21; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+}
+
+// exercicio2();
+
+// 3: Crie um programa que calcula a soma de 1 a 100.
+function exercicio3() {
+  let soma = 0;
+  for(let i = 0; i < 101; i++) {
+    soma += i;
+    console.log(soma)
+  }
+}
+
+// exercicio3();
+
+// 4: Crie um programa que calcula o fatorial de um número.
+function exercicio4() {}
+
+// exercicio4();
+
+// 5: Crie um programa que verifica se um número é primo.
+function exercicio5() {}
+
+// exercicio5();
+
+// 6: Crie um programa que calcula a média de um array de números.
+function exercicio6() {}
+
+// exercicio6();
+
+// 7: Crie um programa que encontra o maior elemento em um array.
+function exercicio7() {}
+
+// exercicio7();
+
+// 8: Crie um programa que inverte um string.
+function exercicio8() {}
+
+// exercicio8();
+
+// 9: Crie um programa que conta o número de vogais em um string.
+function exercicio9() {}
+
+// exercicio9();
+
+// 10: Crie um programa que remove elementos duplicados de um array.
+function exercicio10() {}
+
+// exercicio10();
+
+// 11: Crie um programa que verifica se um string é um palíndromo.
+function exercicio11() {}
+
+// exercicio11();
+
+// 12: Crie um programa que gera os primeiros n números da sequência de Fibonacci.
+function exercicio12() {}
+
+// exercicio12();
+
+// 13: Crie um programa que verifica se um número é par ou ímpar.
+function exercicio13() {}
+
+// exercicio13();
+
+// 14: Crie um programa que calcula a soma dos dígitos de um número.
+function exercicio14() {}
+
+// exercicio14();
+
+// 15: Crie um programa que calcula a área de um triângulo.
+function exercicio15() {}
+
+// exercicio15();
+
+// 16: Crie um programa que ordena um array de números em ordem crescente.
+function exercicio16() {}
+
+// exercicio16();
+
+// 17: Crie um programa que encontra o segundo menor elemento em um array.
+function exercicio17() {}
+
+// exercicio17();
+
+// 18: Crie um programa que calcula a raiz quadrada de um número.
+function exercicio18() {}
+
+// exercicio18();
+
+// 19: Crie um programa que calcula a potência de um número.
+function exercicio19() {}
+
+// exercicio19();
+
+// 20: Crie um programa que calcula o máximo divisor comum (MDC) de dois números.
+function exercicio20() {}
+
+// exercicio20();
+
+// 21: Crie um programa que converte um número decimal para binário.
+function exercicio21() {}
+
+// exercicio21();
+
+// 22: Crie um programa que calcula o comprimento de uma string.
+function exercicio22() {}
+
+// exercicio22();
+
+// 23: Crie um programa que encontra o menor elemento em um array.
+function exercicio23() {}
+
+// exercicio23();
+
+// 24: Crie um programa que verifica se um número é positivo, negativo ou zero.
+function exercicio24() {}
+
+// exercicio24();
+
+// 25: Crie um programa que gera uma sequência de números primos.
+function exercicio25() {}
+
+// exercicio25();
+
+// 26: Crie um programa que calcula a média ponderada de um conjunto de notas.
+function exercicio26() {}
+
+// exercicio26();
+
+// 27: Crie um programa que remove todos os espaços em branco de um string.
+function exercicio27() {}
+
+// exercicio27();
+
+// 28: Crie um programa que encontra o maior elemento em uma matriz.
+function exercicio28() {}
+
+// exercicio28();
+
+// 29: Crie um programa que verifica se um ano é bissexto.
+function exercicio29() {}
+
+// exercicio29();
+
+// 30: Crie um programa que calcula o fatorial de um número usando recursão.
+function exercicio30() {}
+
+// exercicio30();
+
+// 31: Crie um programa que calcula a soma dos elementos em uma matriz.
+function exercicio31() {}
+
+// exercicio31();
+
+// 32: Crie um programa que encontra a diferença entre o maior e o menor elemento em um array.
+function exercicio32() {}
+
+// exercicio32();
+
+// 33: Crie um programa que verifica se um número é múltiplo de outro.
+function exercicio33() {}
+
+// exercicio33();
+
+// 34: Crie um programa que verifica se um número é primo usando o Teorema de Fermat.
+function exercicio34() {}
+
+// exercicio34();
+
+// 35: Crie um programa que calcula a média aritmética de um array de números.
+function exercicio35() {}
+
+// exercicio35();
+
+// 36: Crie um programa que ordena um array de strings em ordem alfabética.
+function exercicio36() {}
+
+// exercicio36();
+
+// 37: Crie um programa que encontra o segundo maior elemento em um array.
+function exercicio37() {}
+
+// exercicio37();
+
+// 38: Crie um programa que calcula o resto da divisão de dois números.
+function exercicio38() {}
+
+// exercicio38();
+
+// 39: Crie um programa que inverte a ordem dos elementos em um array.
+function exercicio39() {}
+
+// exercicio39();
+
+// 40: Crie um programa que verifica se uma string contém outra.
+function exercicio40() {}
+
+// exercicio40();
+
+// 41: Crie um programa que calcula a área de um círculo.
+function exercicio41() {}
+
+// exercicio41();
+
+// 42: Crie um programa que verifica se um número é perfeito.
+function exercicio42() {}
+
+// exercicio42();
+
+// 43: Crie um programa que calcula a soma dos dígitos de um número usando recursão.
+function exercicio43() {}
+
+// exercicio43();
+
+// 44: Crie um programa que verifica se um número é um quadrado perfeito.
+function exercicio44() {}
+
+// exercicio44();
+
+// 45: Crie um programa que encontra o terceiro menor elemento em um array.
+function exercicio45() {}
+
+// exercicio45();
+
+// 46: Crie um programa que conta o número de palavras em um string.
+function exercicio46() {}
+
+// exercicio46();
+
+// 47: Crie um programa que remove todos os elementos duplicados de uma matriz.
+function exercicio47() {}
+
+// exercicio47();
+
+// 48: Crie um programa que verifica se uma string é um pangrama.
+function exercicio48() {}
+
+// exercicio48();
+
+// 49: Crie um programa que verifica se um número é uma potência de dois.
+function exercicio49() {}
+
+// exercicio49();
+
+// 50: Crie um programa que calcula o cosseno de um ângulo em radianos.
+function exercicio50() {}
+
+// exercicio50();
